8ad01ed051f895954b30680c9daa23c44c1f531f
phoby20 <54525527+phoby20@users.noreply.github.com>
2025-03-07 16:53:50 +0900

    chore: バナーの文言及びデザイン修正

--- src/app/pages/Main/MainBanner/index.tsx
+++ src/app/pages/Main/MainBanner/index.tsx
@@ -12,7 +12,7 @@ export default function MainBanner() {
     <div className={styles.container}>
       <Image src={homeBackground} alt="Home Background" style={{ width: '100%', height: 'auto' }}/>
       <div className={styles.banner_title}>
-        <h1>サイト制作を５千円から！</h1>
+        <h1>サイト制作を1ページ５千円から！</h1>
         <h1>紹介サイトは最短3日で納品！</h1>
         <div className={styles.request_button}>
           <Button buttonName="今サイト制作を依頼する" onClick={handleClick} type='tertiary' />
